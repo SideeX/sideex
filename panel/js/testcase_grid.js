1567364202fd3e1e6109df15233b78386d668fcd
Lin Yun Wen <l40303k@gmail.com>
2017-11-04 13:57:36 +0800

    Feature: Add plus icon of adding suite

--- panel/js/testcase_grid.js
+++ panel/js/testcase_grid.js
@@ -294,7 +294,7 @@ function addTestSuite(title, id) {
     // add save icon
     var saveIcon = document.createElement("i");
     saveIcon.classList.add("fa");
-    saveIcon.classList.add("fa-floppy-o");
+    saveIcon.classList.add("fa-download");
     saveIcon.setAttribute("aria-hidden", "true");
     saveIcon.addEventListener("click", clickSaveIcon);
     textDiv.appendChild(saveIcon);
@@ -309,6 +309,7 @@ function addTestSuite(title, id) {
     var pluseIcon = document.createElement("i");
     pluseIcon.classList.add("fa");
     pluseIcon.classList.add("fa-plus");
+    pluseIcon.classList.add("case-plus");
     pluseIcon.setAttribute("aria-hidden", "true");
     pluseIcon.addEventListener("click", clickCasePlusIcon);
     textDiv.appendChild(pluseIcon);
@@ -326,7 +327,8 @@ function addTestSuite(title, id) {
     if (s_suite) {
         s_suite.parentNode.insertBefore(div, s_suite.nextSibling);
     } else {
-        document.getElementById("testCase-grid").appendChild(div);
+        // document.getElementById("testCase-grid").appendChild(div);
+        document.getElementById("testCase-grid").insertBefore(div, document.getElementById("suite-plus"));
     }
 
     cleanSelected();
@@ -510,3 +512,5 @@ function clickSaveIcon(event) {
 function clickSuitePlusIcon(event) {
     document.getElementById("add-testSuite").click();
 }
+
+document.getElementById("suite-plus").addEventListener("click", clickSuitePlusIcon);
